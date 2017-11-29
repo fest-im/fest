407dc35fab58bfaeabc1b7c7d968b0ec0a120b11
Jonas Platte <jplatte+git@posteo.de>
2017-11-29 09:43:38 +0100

    Remove old commented-out code from main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ extern crate hyper;
 extern crate ruma_client;
 extern crate tokio_core;
 extern crate url;
-// extern crate xdg;
 
 #[macro_use]
 mod util;
@@ -19,16 +18,8 @@ mod app;
 mod bg_thread;
 
 use app::App;
-// use std::fs::File;
-// use std::path::Path;
 
 fn main() {
-    // let xdg_dirs      = xdg::BaseDirectories::with_prefix("fest").unwrap();
-    // let data_path     = xdg_dirs.place_data_file("data.yml").unwrap();
-    // TODO: Read settings
-
     let app = App::new();
     app.run();
-
-    // TODO: Save settings
 }
