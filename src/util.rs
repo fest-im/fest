17fcf4cd9354562e7d45515ead0418f0f63505b1
Digital <digidev@digitalville.de>
2018-04-02 22:33:50 +0200

    Enable colored and not colored logging
    Use the log crate for logging. Use the crates fern and chrono as well
    as some custom code to produce colored nice looking logs. Color only
    works in ansi terminals and is enabled by the 'logging_color' feature,
    which is enabled by default.

--- src/util.rs
+++ src/util.rs
@@ -22,3 +22,72 @@ macro_rules! clone {
         }
     );
 }
+
+// ===================== Logging =====================
+extern crate chrono;
+extern crate fern;
+
+/// Sets up the logging front end.
+pub(crate) fn set_up_logging() {
+    use crate::log;
+    set_up_fern_dispatch()
+        // set the default log level
+        .level(log::LevelFilter::Warn)
+        // set module (actually, it's target) specific log levels
+        .level_for("fest", log::LevelFilter::Trace)
+        // output to stdout
+        .chain(::std::io::stdout())
+        .apply().unwrap();
+
+    debug!("finished setting up logging! yay!");
+}
+
+
+/// creates a `fern::Dispatch` object. if the 'logging_color' feature is enabled, it outputs
+/// ansi escape sequencen to color the logs.
+#[cfg(feature = "logging_color")]
+fn set_up_fern_dispatch() -> fern::Dispatch {
+    use util::fern::colors::{Color, ColoredLevelConfig};
+    // configure colors for the whole line
+    let colors_line = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        // we actually don't need to specify the color for debug and info, they are white by default
+        .info(Color::White)
+        .debug(Color::White)
+        // depending on the terminals color scheme, this is the same as the background color
+        .trace(Color::BrightBlack);
+    // configure colors for the name of the level.
+    // since almost all of them are the some as the color for the whole line, we just clone
+    // `colors_line` and overwrite our changes
+    let colors_level = colors_line.clone()
+        .info(Color::Green);
+    // here we set up our fern Dispatch
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{color_line}[{date}][{target}][{level}{color_line}] {message}\x1B[0m",
+                color_line = format_args!("\x1B[{}m", colors_line.get_color(&record.level()).to_fg_str()),
+                date = chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                target = record.target(),
+                level = colors_level.color(record.level()),
+                message = message,
+            ));
+        })
+}
+
+/// creates a `fern::Dispatch` object. if the 'logging_color' feature is enabled, it outputs
+/// ansi escape sequencen to color the logs.
+#[cfg(not(feature = "logging_color"))]
+fn set_up_fern_dispatch() -> fern::Dispatch {
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "[{date}][{target}][{level}] {message}",
+                date = chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                target = record.target(),
+                level = record.level(),
+                message = message,
+            ));
+        })
+}
